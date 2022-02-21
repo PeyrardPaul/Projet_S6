c29617b08a65373dc7d5e70f643786c0684a4814
Paul Peyrard <paul.peyrard17@gmail.com>
2022-02-21 13:28:04 +0100

    j'ai tout mis en php

--- html/inscription.php
+++ html/inscription.php
@@ -4,18 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet"href="../Styles/style.css"type="text/css"media="screen"/>
+	<link rel="stylesheet"href="style.css"type="text/css"media="screen"/>
     <title> Inscription </title>
 </head>
 <body>
 <p> Inscription </p>
 
-<?php
 <label for="nom">Nom(4 à 8 characters):</label>
 <input type="text" id="name" name="name" required
      minlength="3" maxlength="16" size="10"
-	 placeholder="Pseudo";>
+	 placeholder="Pseudo">
 	 <br>
-?>
+<label for="nom">Identifiant :</label>
+<input type="text" id="name" name="name" required
+     minlength="100" maxlength="100" size="10"
+	 placeholder="Adresse mail">
+	 <br>
+<label for="nom">Mot de passe (4 à 12 charactères):</label>
+<input type="text" id="name" name="name" required
+     minlength="4" maxlength="16" size="10"
+	 placeholder="Mot de passe (attention aux majuscules)">
+	 <br>
+<label for="nom">Validez le Mot de passe :</label>
+<input type="text" id="name" name="name" required
+     minlength="4" maxlength="16" size="10"  placeholder="Répétez le mot de passe">
+	
+	 <br>
 </body>
 </html>
---
+++ html/qui_sommes_nous.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"href="../Styles/style.css"type="text/css"media="screen"/>
+    <title>Qui sommes-nous ?</title>
+</head>
+<body>
+<p>Nous sommes un groupe de particulier pour vous aider dans le choix d'une région pour déménager ou pour un voyage à la découverte de ...</p>
+    
+</body>
+</html>
