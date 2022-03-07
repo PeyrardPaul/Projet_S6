bb0f0617374c2a1442608005252f2974cf159c35
Paul Peyrard <paul.peyrard17@gmail.com>
2022-03-07 14:22:52 +0100

    change contact formulaire

--- html/contact.php
+++ html/contact.php
@@ -25,29 +25,36 @@
  <div> <!--ici la grande div qui a le contenu de la page -->
 	<form class="formulaire" action="POST">
 
-	<p><label for="user">Nom :</label>
-	<input type="text" id="user" value="" /></p>
+	<p><strong>Nom :</strong>
+		<input type="text" id="user" value="" />
+	</p>
 
-	<p><label for="email">Email :</label>
-	<input type="text" value="" /></p>
-
-	<p><label for="sexe">Sexe :</label>
-	Homme :<input type="radio" name="H" />
-	Femme :<input type="radio" name="F" /></p>
-
-	<p><label for="motif">Motif :</label>
-	<input type="checkbox" name="motif" /> Aide/Renseignements
-	<input type="checkbox" name="motif" class="pq" /> Demande d'inscription
-	<input type="checkbox" name="motif" class="pq" /> Plainte</p>
-
-
-	<p><label for="message">Message :</label>
-	<textarea rows="5" cols="25"></textarea></p>
-
+	<p><strong>Email :</strong>
+			<input type="text" value="" />
+	</p>
+	<br/>
+	<p><strong>Sexe : </strong>
+		<p style="padding-top:-20px;">
+			Homme<input type="radio" name="H" style="margin-left:-70px;"/>
+			Femme<input type="radio" name="F" style="margin-left:-70px;"/>
+		</p>
+	</p>
+	<br/>
+	<p><strong>Motif :</strong>
+		<p>
+			Aide/Renseignements<input style="margin-left:-80px;" type="checkbox" name="motif" />
+			Demande d'inscription<input style="margin-left:-80px;" type="checkbox" name="motif" class="pq" />
+			Plainte<input style="margin-left:-80px;" type="checkbox" name="motif" class="pq" />
+		</p>
+	</p>
+	<br/>
+	<p><strong>Message :</strong>
+		<p><textarea rows="5" cols="25"></textarea></p>
+	</p>
 	<p><div style="margin-left: 150px;">
 		<input type="submit" value="Envoyer" />
 		<input type="reset" value="Recommencer" />
-	</div></p>
+	</div>
 	</form>
 </div>
 	
