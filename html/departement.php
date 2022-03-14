a5e0bf046dd0c971c373fdeae0e77b0e778cec99
Melissa-pulci <mel.pulci@gmail.com>
2022-03-14 13:46:27 +0100

    dept

--- html/departement.php
+++ html/departement.php
@@ -5,29 +5,28 @@
 ?>
 
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet"href="../Styles/style.css"type="text/css"media="screen"/>
-    <title>Département</title>
-</head>
-<body>
-<div class="bandeau">
-	<img id="logo" src="../images/N-Maps.png" alt="images logo" >
-	<h1><a href="index.php">N-MAPS</a></h1>
-	<ul class="menu">
-		<li><a href="index.php">Accueil</a></li>
-		<li><a href="qui_sommes_nous.php">Qui sommes nous ?</a></li>
-		<li><a href="inscription.php">Inscription</a></li>
-		<li><a href="contact.php">Contact</a></li>
-		<li><a href="departement.php">Département</a></li>
-		<li><a href="connexion.php">Connexion</a></li>
-	</ul>
-</div>
+	<head>
+    	<meta charset="UTF-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<link rel="stylesheet"href="../Styles/style.css"type="text/css"media="screen"/>
+    	<title>Département</title>
+	</head>
 
- <div> <!--dans cette grande div le contenu de la page -->
-    <!-- Ici le contenu de la page -->
+	<body>
+		<div class="bandeau">
+			<img id="logo" src="../images/N-Maps.png" alt="images logo" >
+			<h1><a href="index.php">N-MAPS</a></h1>
+			<ul class="menu">
+				<li><a href="index.php">Accueil</a></li>
+				<li><a href="qui_sommes_nous.php">Qui sommes nous ?</a></li>
+				<li><a href="inscription.php">Inscription</a></li>
+				<li><a href="contact.php">Contact</a></li>
+				<li><a href="departement.php">Département</a></li>
+				<li><a href="connexion.php">Connexion</a></li>
+			</ul>
+		</div>
 
-</div>
-</body>
+
+	</body>
+</html>
