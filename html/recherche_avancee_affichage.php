1a6c945a7ae4f69ed52fc618c8c852b6a74e5ed9
Paul Peyrard <paul.peyrard17@gmail.com>
2022-03-23 10:15:48 +0100

    change

--- html/recherche_avancee_affichage.php
+++ html/recherche_avancee_affichage.php
@@ -57,4 +57,11 @@
     }
         unset($_SESSION['critere']); //vide la session
     ?>
+    <div class="rating"><!--
+   --><a href="#1" title="Donner 1 étoile">☆</a><!--
+   --><a href="#2" title="Donner 2 étoiles">☆</a><!--
+   --><a href="#3" title="Donner 3 étoiles">☆</a><!--
+   --><a href="#4" title="Donner 4 étoiles">☆</a><!--
+   --><a href="#5" title="Donner 5 étoiles">☆</a>
+</div>
 </body>
