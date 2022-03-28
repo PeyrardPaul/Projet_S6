8dfa674ae55d72695560a61057033b642fdc0dfe
Paul Peyrard <paul.peyrard17@gmail.com>
2022-03-28 16:29:00 +0200

    petit changement

--- html/connecter.php
+++ html/connecter.php
@@ -1,7 +1,7 @@
 <?php
-	include 'session.php';
- 	$bdd = getBD(); 
-?>
+include 'session.php'; ?>
+<?php ?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -15,68 +15,84 @@
         <img id="logo" src="../images/N-Maps.png" alt="images logo" >
         <h1><a href="index.php">N-MAPS</a></h1>
         <ul class="menu">
-		<?php
-            // si l'utilisateur n'est pas connecté 
-            if(!isset($_SESSION['user'])) {
-                echo'<li><a href="index.php">Accueil</a></li>';
-                echo'<li><a href="recherche_simple.php">Recherche simple</a></li>';
-                echo'<li><a href="connexion.php">Connexion </a></li>';
-            } else if(isset($_SESSION['user'])) {
-                // si l'utilisateur est connecté 
-                echo'<li><a href="index.php">Accueil</a></li>';
-                echo'<li><a href="recherche_simple.php">Recherche simple</a></li>';
-                echo'<li><a href="recherche_avancee.php">Recherche avancée</a></li>';
-                echo"<li><a href='deconnexion.php'>Déconnexion</a></li>";
-                echo "<li>Bonjour ".$_SESSION['user'][4]."</li>";
-            }
-        ?>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="qui_sommes_nous.php">Qui sommes nous ?</a></li>
+            <li><a href="inscription.php">Inscription</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="recherche_simple.php">Recherche simple</a></li>
+            <li><a href="recherche_avancee.php">Recherche avancée</a></li>
+
+			<li>
+                <?php
+		                if(isset($_SESSION['user'])) {
+
+			                echo "<li><a href='deconnexion.php'>Se déconnecter</a></li>";
+							echo "<li>Bonjour ".$_SESSION['user'][4]."</li>";
+
+		                }
+		                else {
+			                echo "<li><a href='connexion.php'>Se connecter </a></li>";
+		                }?></li>
         </ul>
     </div>
     <?php
-	if(isset($_POST['login'])){	
-		$pseudo = $_POST['pseudo'];
-		$password = $_POST['password'];
-		$rep = $bdd -> query("select * from users where pseudo ='{$pseudo}' and password ='{$password}'");			//echo("<meta http-equiv='refresh' content='1; url=http://localhost/PEYRARD/index.php '>");
-		while ($mat = $rep->fetch()) {
-			$_SESSION['user']=array($mat['user_id'],$mat['code_dep'],$mat['nom'],$mat['prenom'],$mat['pseudo'],$mat['password'],$mat['adresse_email'],$mat['adresse'],$mat['type'],$mat['password_reset']);
-		}
-	}
-			/*try{
-			$stmt = $bdd->prepare("SELECT *, COUNT(*) AS numrows FROM users WHERE pseudo = :pseudo");
-			$stmt->execute(['pseudo'=>$pseudo]);
-			$row = $stmt->fetch();
-			if($row['numrows'] > 0){
+
+if(isset($_POST['login'])){
+		
+	$pseudo = $_POST['pseudo'];
+	$password = $_POST['password'];
+
+	try{
+
+		$stmt = $bdd->prepare("SELECT *, COUNT(*) AS numrows FROM users WHERE pseudo= :pseudo");
+		$stmt->execute(['pseudo'=>$pseudo]);
+		$row = $stmt->fetch();
+		if($row['numrows'] > 0){
 				if(password_verify($password, $row['password'])){
 					if($row['type']==1){
 						$_SESSION['admin'] = $row['user_id'];
-					} else {
-						$_SESSION['user'] = $row['user_id'];
+						//header('location: administrateur/home.php');
 					}
-				} else{
+					else
+					{
+						$_SESSION['user'] = $row['user_id'];
+					 //	header('location: index.php');
+					}
+				}
+				else{
 					$_SESSION['error'] = 'Mot de passe incorrect';
 				}
-			} else{
-			$_SESSION['error'] = 'pseudo non trouvé';
-			}
-		} catch(PDOException $e){
-			echo "Problème de connection: " . $e->getMessage();
 		}
-	} else{
-		$_SESSION['error'] = 'erreur';
+		else{
+			$_SESSION['error'] = 'pseudo non trouvé';
+		}
 	}
-	$stmt-> closeCursor();
-	header('location: connexion.php');
-	*/
-	?>	
+	catch(PDOException $e){
+		echo "Problème de connection: " . $e->getMessage();
+	}
+
+}
+else{
+	$_SESSION['error'] = 'erreur';
+}
+
+		// $pseudo=$_POST['pseudo'];
+		// $mdp=$_POST['password'];
+		
+		// if($pseudo=="" or $mdp=="") {
+		// 	echo "mail ou mot de passe vide";
+		// 	echo("<meta http-equiv='refresh' content='1; url=connexion.php?pseudo=".$pseudo." '>");
+		// }
+		// else {
+		// 	$rep = $bdd -> query("select * from users where pseudo ='{$pseudo}' and password ='{$mdp}'");
+		// 	while ($mat = $rep->fetch()) {
+		// 		echo "<h2>Connexion réussie, profitez au maximum de notre site maintenant !</h2>";
+		// 		$_SESSION['user']=array($mat['user_id'],$mat['code_dep'],$mat['nom'],$mat['prenom'],$mat['pseudo'],$mat['adresse_email'],$mat['type'],$mat['code_activation'],$mat['code_reset']);
+        //     }
+		// 	echo("<meta http-equiv='refresh' content='1; url=index.php '>");
+		// }
+		$stmt-> closeCursor();
+		header('location: connexion.php');
+		?>	
 	</body>
-
-
-	<footer>
-        <p>N-Maps &copy; 2022 
-        -   <a href="qui_sommes_nous.php"> Qui sommes nous ? </a>   
-        -   <a href="contact.php"> Nous contacter </a>  
-        -   <a href="commentaire.php"> Espace commentaires</a>  
-        </p> 
-    </footer>
-	
 </html>
