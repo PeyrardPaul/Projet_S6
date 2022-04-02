50a007a9525becea8bf41f4df73ccd0e0ca05c37
Shelssiah <shelmyassiah@gmail.com>
2022-04-02 05:07:23 +0200

    modifs

--- html/connexion.php
+++ html/connexion.php
@@ -6,12 +6,14 @@
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet"href="../Styles/style.css"type="text/css"media="screen"/>
+<link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
 <title>N-Maps</title>
 <style>
 </style>
 </head>
 
-<body>
+<body class="hold-transition login-page">
 
 <div class="bandeau"> <!--ici le bandeau haut de page -->
     <img id="logo" src="../images/N-Maps.png" alt="images logo" >
@@ -35,37 +37,51 @@
         ?>
     </ul>
 </div>
+<div class="login-box" style="margin-top:-50px;">
 
-	<h2 style="text-align:center;margin-bottom:-150px;margin-top:90px;"> Se connecter ou <a class="retour" href="inscription.php" style="margin-left:auto;margin-right:auto;width:200px;">s'inscrire</a></h2>
+	<h2><p class="box-msg"> Se connecter </p></h2>
+    
+    <div class="box">
+        <form method="post" action="connecter.php" autocomplete="on">
 
-	<form method="post" action="connecter.php" autocomplete="on">
-		<div class="pseudo">
-			<label for="pseudo"> Pseudo </label> :
-			<input type="pseudo"  name="pseudo" value="" placeholder="Saisir pseudo" required/>
-		</div>
+            <div class="form-group has-feedback">
+                <label for="pseudo"> Pseudo </label> :
+                <input type="pseudo" class="form-control" name="pseudo" value="" placeholder="Saisir pseudo" required/>
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+            </div>
 
-		<div class="password">
-			<label for="password" name="password"> Mot de passe </label> :
-			<input type="password" name="password" placeholder="Saisir password" required/> 
-		</div>
+            <div class="form-group has-feedback">
+                <label for="password" name="password"> Mot de passe </label> :
+                <input type="password" class="form-control" name="password" placeholder="Saisir password" required/>
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span> 
+            </div>
 
-		<p style="text-align:center;margin-top:15px;"> 
-			<a id="bleu" href="mdp_oublie.php"> 
-				Mot de passe oublié ? 
-			</a>
-		</p>
-
-		<input type="submit" class="login" name="login" value="Me connecter"/>
-	</form>
-
-
-
-	<footer>
+            <p style="text-align:center;margin-top:15px;"> 
+                <a id="bleu" href="mdp_oublie.php"> 
+                    Mot de passe oublié ? 
+                </a>
+            </p>
+            <div class="row">
+    			<div class="col-xs-5">
+          			<button type="submit" class="btn btn-warning btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i>Me connecter</button>
+        		</div>
+      		</div>
+        </form>
+        <br/>
+        <a href="inscription.php" class="text-center">s'inscrire</a><br>
+      <a href="index.php"><i class="fa fa-home"></i> Accueil</a>
+    </div>
+    
+    </div>
+</body>
+<br/>
+<br/>
+<br/>
+	<footer class="footer">
         <p>N-Maps &copy; 2022 
         -   <a href="qui_sommes_nous.php"> Qui sommes nous ? </a>   
         -   <a href="contact.php"> Nous contacter </a>  
         </p> 
     </footer>
 
-</body>
 </html>
--- html/inscription.php
+++ html/inscription.php
@@ -13,9 +13,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet"href="../Styles/style.css"type="text/css"media="screen"/>
+    <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
     <title> Inscription </title>
 </head>
-<body>
+<body class="hold-transition register-page">
 <div class="bandeau"> <!--ici le bandeau haut de page -->
         <img id="logo" src="../images/N-Maps.png" alt="images logo" >
         <h1><a href="index.php">N-MAPS</a></h1>
@@ -38,68 +40,101 @@
         </ul>
     </div>
 <div>
-<form method=Post action="enregistrement.php" autocomplete=ON>
-<div class="marg">
-<p><strong>Nom :</strong>
-    <input type="text" id="name" name="n" required
-     maxlength="20" size="10"
-    placeholder="Nom" style="margin-bottom:20px;" value=<?php echo "'".$_GET['n']."'"?>>
-</p>
-    <br/>
-<p><strong>Prénom :</strong>
-    <input type="text" id="name" name="p" required
-     maxlength="50" size="10"
-    placeholder="Prénom" style="margin-bottom:20px;" value=<?php echo "'".$_GET['p']."'"?>>
-</p>
-    <br/>
-<p><strong>Pseudo :</strong>
-    <input type="text" id="name" name="psd" required
-     maxlength="50" size="10"
-    placeholder="Pseudo" style="margin-bottom:20px;" value=<?php echo "'".$_GET['psd']."'"?>>
-</p>
-    <br/>
-<p><strong>Département :</strong>
-    <input type="text" id="name" name="dep" required
-     maxlength="2" size="10"
-    placeholder="Département Ex: 34" style="margin-bottom:20px;" value=<?php echo "'".$_GET['dep']."'"?>>
-</p>
-    <br/>
-<p><strong>Adresse email :</strong>
-    <input type="text" id="name" name="mail" required
-     maxlength="50" size="10"
-    placeholder="Adresse mail" style="margin-bottom:20px;" value=<?php echo "'".$_GET['mail']."'"?>>
-</p>
-    <br/>
-<p><strong>Adresse :</strong>
-    <input type="text" id="name" name="adr" required
-     maxlength="50" size="10"
-    placeholder="Adresse" style="margin-bottom:20px;" value=<?php echo "'".$_GET['adr']."'"?>>
-</p>
-    <br/>
-<p><strong>Mot de passe :</strong>
-    <input type="password" id="name" name="mdp1" required
-    minlength="4" maxlength="20" size="10"
-    placeholder="Mot de passe" style="margin-bottom:20px;">
-</p>
-    <br/>
-<p><strong>Validez le Mot de passe :</strong>
-    <input type="password" id="name" name="mdp2" required
-    minlength="4" maxlength="20" size="10"  placeholder="Répétez le mot de passe" style="margin-bottom:20px;">
-</p>
-<br/>
-</div>
-<p><div style="margin-left: 150px;">
-		<input type="submit" value="Envoyer" />
-</div></p>
-</form>
-</div>
 
-    <footer>
-        <p>N-Maps &copy; 2022 
-        -   <a href="qui_sommes_nous.php"> Qui sommes nous ? </a>   
-        -   <a href="contact.php"> Nous contacter </a>   
-        </p> 
-    </footer>
+<div class="register-box" style="margin-top:-50px;">
+
+<h2><p class="box-msg"> Enregistrement </p></h2>
+    
+    <div class="box">
+            <form method=Post action="enregistrement.php" autocomplete=ON>
+                
+            <div class="form-group has-feedback">
+            <label for="nom">Nom </label>:
+                <input type="text" class="form-control" id="name" name="n" required
+                maxlength="20" size="10"
+                placeholder="Nom"  >
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+            </div>
+             
+            <div class="form-group has-feedback">
+            <label for="prenom">Prénom </label>:
+                <input type="text" id="name" class="form-control" name="p" required
+                maxlength="50" size="10"
+                placeholder="Prénom">
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+            </div>
+            
+            <div class="form-group has-feedback">
+            <label for="pseudo">Pseudo</label> :
+                <input type="text" id="name" class="form-control" name="psd" required
+                maxlength="50" size="10"
+                placeholder="Pseudo"  >
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+            </div>
+             
+            <div class="form-group has-feedback">
+            <label for="dep">Département</label> :
+                <input type="text" class="form-control" id="name" name="dep" required
+                maxlength="2" size="10"
+                placeholder="Département Ex: 34"  >
+                <span class="glyphicon glyphicon-globe form-control-feedback"></span>
+            </div>
+            
+            <div class="form-group has-feedback">
+            <label for="mail">Email</label> :
+                <input type="text" id="name" class="form-control" name="mail" required
+                maxlength="50" size="10"
+                placeholder="Adresse mail"  >
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+                
+            <div class="form-group has-feedback">
+            <label for="adr">Adresse</label> :
+                <input type="text" id="name" class="form-control" name="adr" required
+                maxlength="50" size="10"
+                placeholder="Adresse" >
+                <span class="glyphicon glyphicon-bed form-control-feedback"></span>
+            </label>
+                
+            <div class="form-group has-feedback">
+            <label for="mdp1">Mot de passe </label>:
+                <input type="password" id="name" class="form-control" name="mdp1" required
+                minlength="4" maxlength="20" size="10"
+                placeholder="Mot de passe">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+                
+            <div class="form-group has-feedback">
+            <label for="mdp2">Confirmez</label> :
+                <input type="password" id="name" class="form-control" name="mdp2" required
+                minlength="4" maxlength="20" size="10"  placeholder="Répétez le mot de passe" >
+                <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+            </div>
+            <br/>
+
+            </div>
+
+            <hr>
+          <div class="row">
+          <div class="col-xs-5">
+                <button type="submit" class="btn btn-warning btn-block btn-flat" name="signup"><i class="fa fa-pencil"></i>Envoyer</button>
+            </div>
+          </div>
+    
+            </form>
+            <br>
+      <a href="connexion.php">J'ai déjà un compte</a><br>
+      <a href="index.php"><i class="fa fa-home"></i> Acceuil</a>
+        </div>
+    </div>
 
 </body>
+<br/>
+<br/>
+<footer class="footer">
+        <p>N-Maps &copy; 2022 
+        -   <a href="qui_sommes_nous.php"> Qui sommes nous ? </a>   
+        -   <a href="contact.php"> Nous contacter </a>  
+        </p> 
+    </footer>
 </html>
