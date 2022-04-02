c1c91181c155d3f289d3cfbad044f99e633ba313
Shelssiah <shelmyassiah@gmail.com>
2022-04-02 17:34:36 +0200

    modifs

--- html/connecter.php
+++ html/connecter.php
@@ -99,7 +99,7 @@ else{
 }*/
 		?>	
 	</body>
-	<footer><!--ici le pied de page -->
+	<footer class="footer"><!--ici le pied de page -->
         <p>N-Maps &copy; 2022 
         -   <a href="qui_sommes_nous.php"> Qui sommes nous ? </a>   
         -   <a href="contact.php"> Nous contacter </a>  
